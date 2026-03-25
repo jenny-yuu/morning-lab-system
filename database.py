0b3ba830b4ca7dcf582e653629e08e05ebbe41e0
Jenny <jenny@example.com>
2026-03-26 01:52:42 +0800

    Fix: Force DB refresh for schema update (status column)

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./morning_lab.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./morning_lab_v3.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
